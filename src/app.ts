283eef78f72d03438abf62591fe89d590d16f5b1
Pedro Henrique Vicente Ferreira <anddev007pe@gmail.com>
2021-07-14 17:40:54 -0300

    created created error handling

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,19 @@
 import 'reflect-metadata';
 import express from 'express';
+import 'express-async-errors';
 
 import { routes } from './routes';
 
 import './database';
+
+import { middlewareError } from './middlewares/Error/Error';
 
 const app = express();
 
 app.use(express.json());
 
 app.use(routes);
+
+app.use(middlewareError);
 
 export { app };
